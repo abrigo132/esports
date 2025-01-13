0cf0990fa7e3771f385f55179354c603442dafdc
abrigo132 <garipotter97@gmail.com>
2025-01-13 17:21:54 +0500

    create profile view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+
+class ProfileView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = "id"
+    permission_classes = [IsAuthenticated]
