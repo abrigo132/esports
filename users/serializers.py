a42a88aaaa1132de35a8515562a6ee09b6bfe02e
abrigo132 <garipotter97@gmail.com>
2025-01-13 19:25:06 +0500

    create serializer for user register

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import User
+from .services import create_user
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -26,3 +27,28 @@ class UserSerializer(serializers.ModelSerializer):
             "last_online",
             "is_online",
         ]
+
+
+class UserSignUpSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "password", "email", "role"]
+
+    def create(self, validate_data: dict) -> User:
+        return create_user(
+            username=validate_data["username"],
+            email=validate_data["email"],
+            password=validate_data["password"],
+            role=validate_data.get(User.Role.PLAYER),
+        )
+
+    def validate_role(self, value: str) -> str:
+        if not value:
+            return User.Role.PLAYER
+        if value not in User.Role.values:
+            raise serializers.ValidationError(
+                f"Недопустимая роль. Допустимые значения: {', '.join(User.Role.values)}"
+            )
+        return value
