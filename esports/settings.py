850bb784af0365a3e11ec6e08040c1ec1f727b04
abrigo132 <garipotter97@gmail.com>
2025-01-13 17:38:24 +0500

    add rest framework in apss

--- esports/settings.py
+++ esports/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework" "api.apps.ApiConfig",
+    "rest_framework",
+    "api.apps.ApiConfig",
     "users.apps.UsersConfig",
 ]
 
