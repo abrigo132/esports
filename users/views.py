88e968df333c7c4fac2c83300fa7e45dbea34b53
abrigo132 <garipotter97@gmail.com>
2025-01-13 19:26:25 +0500

    create register user view

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserSignUpSerializer
 
 
 class ProfileView(generics.RetrieveAPIView):
@@ -10,3 +10,7 @@ class ProfileView(generics.RetrieveAPIView):
     serializer_class = UserSerializer
     lookup_field = "id"
     permission_classes = [IsAuthenticated]
+
+
+class SignUpView(generics.CreateAPIView):
+    serializer_class = UserSignUpSerializer
