648405e8e15dbc944a422a68cbf30c76684c1e75
abrigo132 <garipotter97@gmail.com>
2025-01-13 19:26:11 +0500

    add path register

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ProfileView
+from .views import ProfileView, SignUpView
 
 urlpatterns = [
     path("profile/<int:id>/", ProfileView.as_view(), name="profile"),
+    path("register", SignUpView.as_view(), name="register"),
 ]
