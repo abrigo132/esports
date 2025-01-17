85f44da8b19aed45527290edf214de5750c2f2b0
abrigo132 <garipotter97@gmail.com>
2025-01-17 11:07:30 +0500

    create path for users list

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ProfileView, SignUpView, SocialLoginView
+from .views import ProfileView, SignUpView, SocialLoginView, ListUsersView
 
 urlpatterns = [
     path("profile/<int:id>/", ProfileView.as_view(), name="profile"),
     path("register/", SignUpView.as_view(), name="register"),
     path("social/login/", SocialLoginView.as_view(), name="social_login"),
+    path("users/list", ListUsersView.as_view(), name="users list"),
 ]
