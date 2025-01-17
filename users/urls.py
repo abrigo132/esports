8a4155294eb54585e396d7455b386a7de22624ad
abrigo132 <garipotter97@gmail.com>
2025-01-17 10:45:46 +0500

    add path for social login

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProfileView, SignUpView
+from .views import ProfileView, SignUpView, SocialLoginView
 
 urlpatterns = [
     path("profile/<int:id>/", ProfileView.as_view(), name="profile"),
-    path("register", SignUpView.as_view(), name="register"),
+    path("register/", SignUpView.as_view(), name="register"),
+    path("social/login/", SocialLoginView.as_view(), name="social_login"),
 ]
