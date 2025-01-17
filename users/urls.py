de554619c23eb36a4af42e637c2050711e0ca6a7
abrigo132 <garipotter97@gmail.com>
2025-01-17 11:19:43 +0500

    add / for users/list/

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("profile/<int:id>/", ProfileView.as_view(), name="profile"),
     path("register/", SignUpView.as_view(), name="register"),
     path("social/login/", SocialLoginView.as_view(), name="social_login"),
-    path("users/list", ListUsersView.as_view(), name="users list"),
+    path("users/list/", ListUsersView.as_view(), name="users list"),
 ]
