7ddf14d5ce951f5d8f08fcd1a152f1710ad03d0e
abrigo132 <garipotter97@gmail.com>
2025-01-17 11:58:44 +0500

    add profile serializer

--- users/serializers.py
+++ users/serializers.py
@@ -5,6 +5,10 @@ from .services import create_user
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для просмотра информации пользователя
+    """
+
     class Meta:
         model = User
         fields = [
@@ -29,6 +33,10 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserSignUpSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для регистрации пользователей
+    """
+
     password = serializers.CharField(write_only=True)
 
     class Meta:
@@ -51,3 +59,46 @@ class UserSignUpSerializer(serializers.ModelSerializer):
                 f"Недопустимая роль. Допустимые значения: {', '.join(User.Role.values)}"
             )
         return value
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для личного кабинета и обновления данных пользователя
+    """
+
+    description = serializers.CharField(max_length=1000, required=False)
+    first_name = serializers.CharField(max_length=50, required=False)
+    last_name = serializers.CharField(max_length=50, required=False)
+    role = serializers.CharField(max_length=20, required=False)
+    avatar = serializers.ImageField(required=False)
+    birth_date = serializers.DateField(required=False)
+    city = serializers.CharField(max_length=50, required=False)
+    country = serializers.CharField(max_length=50, required=False)
+    favorite_games = serializers.CharField(max_length=50, required=False)
+    main_game = serializers.CharField(max_length=50, required=False)
+    rank = serializers.CharField(max_length=50, required=False)
+    team = serializers.CharField(max_length=100, required=False)
+    steam = serializers.URLField(required=False)
+    twitch = serializers.URLField(required=False)
+    youtube = serializers.URLField(required=False)
+
+    class Meta:
+        model = User
+        fields = [
+            "description",
+            "first_name",
+            "last_name",
+            "role",
+            "avatar",
+            "birth_date",
+            "city",
+            "country",
+            "favorite_games",
+            "main_game",
+            "rank",
+            "team",
+            "steam",
+            "twitch",
+            "youtube",
+        ]
+        read_only_fields = ["id", "username", "email"]
