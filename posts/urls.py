724cf04c360af41213a4209cf7483a3d20e4ae6e
abrigo132 <garipotter97@gmail.com>
2025-01-17 15:39:24 +0500

    add path posts

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,5 @@ from .views import ListPostsView, DetailPostView
 
 urlpatterns = [
     path("posts/<int:id>/", DetailPostView.as_view(), name="detail post"),
-    path("posts/", ListPostsView.as_view(), name="list posts")
+    path("posts/", ListPostsView.as_view(), name="list posts"),
 ]
