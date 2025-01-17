0144969e01c8961ba6b24384adf6e5b50b4748cc
abrigo132 <garipotter97@gmail.com>
2025-01-17 15:42:03 +0500

    change path for detail view post

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ListPostsView, DetailPostView
 
 urlpatterns = [
-    path("posts/<int:id>/", DetailPostView.as_view(), name="detail post"),
+    path("posts/<int:post_id>/", DetailPostView.as_view(), name="detail post"),
     path("posts/", ListPostsView.as_view(), name="list posts"),
 ]
