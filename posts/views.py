333895b85d50d9a46cc95881695b807ceb464b6e
abrigo132 <garipotter97@gmail.com>
2025-01-17 15:32:52 +0500

    create list posts view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+
+
+class ListPostsView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
