7ddd62122abeb91fbd760edec3ec53cab6146a00
abrigo132 <garipotter97@gmail.com>
2025-01-20 18:57:38 +0500

    add paths for comments

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path
 
-from .views import ListPostsView, DetailPostView
+from .views import (
+    ListPostsView,
+    DetailPostView,
+    ListPostsCommentsView,
+    CommentListCreateView,
+    CommentDetailView,
+)
 
 urlpatterns = [
-    path("<int:post_id>/", DetailPostView.as_view(), name="detail post"),
+    path("comments/", CommentListCreateView.as_view(), name="list comments"),
+    path("comments/<int:pk>/", CommentDetailView.as_view(), name="detail comment"),
+    path(
+        "<int:post_id>/comments/",
+        ListPostsCommentsView.as_view(),
+        name="list posts with comments",
+    ),
     path("", ListPostsView.as_view(), name="list posts"),
+    path("<int:pk>/", DetailPostView.as_view(), name="detail post")
+
 ]
