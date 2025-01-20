db93310e7b63bde3e979652f3de47a406a0dbd47
abrigo132 <garipotter97@gmail.com>
2025-01-20 18:53:33 +0500

    create model Comment

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import ForeignKey
 
 from users.models import User
 
@@ -21,6 +22,27 @@ class Post(models.Model):
 
     def __str__(self):
         return f"Post by {self.user.username} at {self.created_at}"
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Comment(models.Model):
+    """
+    Модель для комментариев к определённому посту
+    """
+
+    user = ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    post = ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    text = models.CharField(max_length=1000, null=False)
+    parent_comment = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="replies"
+    )  # Вложенные комментарии
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} in {self.post.id}"
 
     class Meta:
         ordering = ["-created_at"]
