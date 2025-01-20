1dafc483b09d92983da75eca318447947787b420
abrigo132 <garipotter97@gmail.com>
2025-01-20 18:57:56 +0500

    create view for comments

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from .models import Post
-from .serializers import PostSerializer
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
 
 class ListPostsView(generics.ListAPIView):
@@ -18,3 +18,37 @@ class DetailPostView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class ListPostsCommentsView(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        post_id = self.kwargs["post_id"]
+        return Comment.objects.filter(post_id=post_id)
+
+    def perform_create(self, serializer):
+        # Привязываем комментарий к текущему пользователю и посту
+        post_id = self.kwargs["post_id"]
+        serializer.save(user=self.request.user, post_id=post_id)
+
+
+class CommentListCreateView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [
+        IsAuthenticatedOrReadOnly
+    ]  # Только аутентифицированные пользователи могут создавать комментарии
+
+    def perform_create(self, serializer):
+        # Привязываем комментарий к текущему пользователю
+        serializer.save(user=self.request.user)
+
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [
+        IsAuthenticatedOrReadOnly
+    ]  # Только автор может редактировать или удалять комментарий
